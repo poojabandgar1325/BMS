f45dcdf8ebd1a381abaed7461b614734edecea2c
poojabandgar1325 <poojabandgar1325@gmail.com>
2022-11-27 12:10:41 +0530

    WPF

---
+++ BankManagementSystem/BMSWPF/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMSWPF.Models
+{
+    public class User
+    {
+        [Key]
+        public int AccountNo { get; set; }
+        public string Name { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Address { get; set; }
+        public string Country { get; set; }
+        public string State { get; set; }
+        public string EmailID { get; set; }
+        public string PAN { get; set; }
+        public long ContactNo { get; set; }
+        public DateTime DOB { get; set; }
+        public string AccountType { get; set; }
+        public int Role { get; set; }
+        public string JWT { get; set; }
+
+    }
+}
---
+++ BankManagementSystem/BMSWPF/Views/UserRegistration.xaml.cs
@@ -0,0 +1,70 @@
+﻿using BMSWPF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BMSWPF.Views
+{
+    /// <summary>
+    /// Interaction logic for UserRegistration.xaml
+    /// </summary>
+    public partial class UserRegistration : Window
+    {
+        public UserRegistration()
+        {
+            InitializeComponent();
+        }
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:5001/");
+
+            client.DefaultRequestHeaders.Accept.Add(
+               new MediaTypeWithQualityHeaderValue("application/json"));
+            var user = new User();
+
+          //  user.Id = int.Parse(txtId.Text);
+            user.Name = txtName.Text;
+            user.UserName = txtUsername.Text;
+            user.Password = txtPassword.Text;
+            user.Address = txtAddress.Text;
+            user.Country = txtCountry.Text;
+            user.State = txtState.Text;
+            user.EmailID = txtEmail.Text;
+            user.PAN = txtPan.Text;
+            user.ContactNo = long.Parse(txtContactNo.Text);
+            user.DOB = DateTime.Parse(txtDOB.Text);
+            user.AccountType = txtAccountType.Text;
+
+            var response = client.PostAsJsonAsync("api/Users", user).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                MessageBox.Show("Employee Added");
+                //txtId.Text = "";
+                //txtName.Text = "";
+                //txtAddress.Text = "";
+                //txtDesignation.Text = "";
+                //BindEmployeeList();
+            }
+            else
+            {
+                MessageBox.Show("Error Code" + response.StatusCode + " : Message - " + response.ReasonPhrase);
+            }
+        }
+
+    }
+}
